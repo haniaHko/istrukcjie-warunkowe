e4ca898132ff8321459e5c9f05fa4fc6a26c091e
Marek Wenta <marek.wenta2009@gmail.com>
2023-10-09 10:30:01 +0200

    fddasda

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ liczba = int(input("podaj liczbę od 1 do 5"))
 
 if liczba == 1:
     print('jeden')
-
+elif liczba == 2:
+    print('dwa')
+elif liczba == 3:
+    print('trzy')
+elif liczba == 4:
+    print('cztery')
+elif liczba == 5:
+    print('pięć')
+else:
 print('koniec1')
